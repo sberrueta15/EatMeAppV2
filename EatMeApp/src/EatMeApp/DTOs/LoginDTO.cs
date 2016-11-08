1bb2e03a91191c5acd88dd61b46bd48da2eb5a31
Brian Hardy <hardyr.93@gmail.com>
2016-11-08 19:17:57 -0300

    Login not working

---
+++ EatMeApp/src/EatMeApp/DTOs/LoginDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EatMeApp.DTOs
+{
+    public class LoginDTO
+    {
+        /// <summary>
+        /// User email or login name
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// User password
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
